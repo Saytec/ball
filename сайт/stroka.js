adbc7aaee3af8f7110302554ff48c654995657c4
Бочков Максим <ocmax@mail.ru>
2014-12-27 00:43:13 +0300

    Катя

--- сайт/stroka.js
+++ сайт/stroka.js
@@ -1,2 +1,2 @@
 // Java Document
- document.write( "<font color=#ffffff>Dream Team –  Big Black Boots    39:31</font>"); 
+ document.write( "<font color=#ffffff>Dream Team – Big Black Boots    39:31</font>");
